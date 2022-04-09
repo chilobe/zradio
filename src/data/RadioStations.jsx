5cd8885d7de0021d2817c1daf1236551924a8744
Chilobe Mwanza <chilobe.mwanza@whistl.co.uk>
2022-04-09 22:14:16 +0100

    more stations

--- src/data/RadioStations.jsx
+++ src/data/RadioStations.jsx
@@ -1,5 +1,11 @@
 import HotFmIcon from '../img/hot_fm.jpg';
 import iWaveFmIcon from '../img/iwave_fm.jpg';
+import phoenixFmIcon from '../img/radio_phoenix.png';
+import sunFmIcon from '../img/sun_fm.jpg';
+import breezeFmIcon from '../img/breeze_fm.jpg';
+import radioChikuniIcon from '../img/radio_chikuni.jpg';
+import rockFmIcon from '../img/rock_fm.jpg';
+import christianVoiceIcon from '../img/christian_voice.webp'
 
 const RadioStations = [
     {
@@ -17,6 +23,54 @@ const RadioStations = [
         ],
         icon: iWaveFmIcon,
         id: 1,
+    },
+    {
+        name: "Radio Phoenix 89.5",
+        urls: [
+            "https://23553.live.streamtheworld.com/RADIO_PHOENIXAAC_SC"
+        ],
+        icon: phoenixFmIcon,
+        id: 2,
+    },
+    {
+        name: "Sun FM",
+        urls: [
+            "http://11233.cloudrad.io:9102/live"
+        ],
+        icon: sunFmIcon,
+        id: 3,
+    },
+    {
+        name: "Breeze FM Chipata",
+        urls: [
+            "https://s47.myradiostream.com/9934/listen.mp3"
+        ],
+        icon: breezeFmIcon,
+        id: 4,
+    },
+    {
+        name: "Radio Chikuni",
+        urls: [
+            "http://centauri.shoutca.st:8102/stream"
+        ],
+        icon: radioChikuniIcon,
+        id: 5,
+    },
+    {
+        name: "965 Rock FM",
+        urls: [
+            "http://99.198.118.250:8238/stream"
+        ],
+        icon: rockFmIcon,
+        id: 6,
+    },
+    {
+        name: "Radio Christian Voice",
+        urls: [
+            "http://zas2.ndx.co.za/proxy/cvglobal?mp=/stream"
+        ],
+        icon: christianVoiceIcon,
+        id: 7,
     }
 ];
 
